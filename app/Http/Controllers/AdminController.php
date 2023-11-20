bdf56dd025a15384553529a99f122db6f1488933
Alya Kamran <alyakamran82@gmail.com>
2023-11-20 17:26:14 +0800

    up until update password page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -58,7 +59,29 @@ class AdminController extends Controller
     } //End Method
 
     public function ChangePassword(){
-        return view(admin.admin_change_password);
+        return view('admin.admin_change_password');
+    } //End Method
+
+    public function UpdatePassword(Request $request){
+
+        $validateData = $request->validate([
+            'oldpassword' => 'required',
+            'newpassword' => 'required',
+            'confirmpassword' => 'required|same:newpassword',
+        ]);
+
+        $hashedPassword = Auth::user()->password;
+        if (Hash::check($request->oldpassword, $hashedPassword)) {
+            $users = User::find(Auth::id());
+            $users->password = bcrypt($request->newpassword);
+            $users->save();
+
+            session()->flash('message', 'Password Updated Successfully');
+            return redirect()->back();
+        } else {
+            session()->flash('message', 'Old Password is not match');
+            return redirect()->back();
+        }
     } //End Method
 }
 
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::controller(AdminController::class)->group(function () {
     Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
     Route::post('/store/profile', 'StoreProfile')->name('store.profile');
     Route::get('/change/password', 'ChangePassword')->name('change.password');
+    Route::post('/update/password', 'UpdatePassword')->name('update.password');
 });
 
 
